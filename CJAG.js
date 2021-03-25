381c1233536c09b36f831dea50453f0b949540f7
Kristian B. Jacobsen <81099824+Krisbjac@users.noreply.github.com>
2021-03-25 21:26:53 +0100

    basic functionality added

--- CJAG.js
+++ CJAG.js
@@ -1,1 +1,16 @@
-console.log('Jazz')
+const classicAlbums = ['Miles Davis', 'Keith Jarret', 'Jan Garbarek', 
+'Dizzie Gillespie', 'Jelly Roll Morton', 'Wayne Shorter'
+
+]
+const suggestion = ['Cool people', 'No one', 'Everyone', 'My dad', 'Weirdos', 'Idiots'
+
+]
+const verb = [' listens to ', ' avoids ', ' hates ', ' loves ', 
+' dances while listening to ', ' enjoys a glass of whine and '
+
+]
+var randomAlbumNumber = Math.floor(Math.random() * 5)
+var randomSuggestionNumber = Math.floor(Math.random() * 5)
+var randomVerbNumber = Math.floor(Math.random() * 5)
+
+console.log(suggestion[randomSuggestionNumber] + verb[randomVerbNumber] + classicAlbums[randomAlbumNumber])
